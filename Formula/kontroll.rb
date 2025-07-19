6029d08a5f0826890cbf31260e41497b19e32910
John Paul Ashenfelter <john@ashenfelter.com>
2025-07-19 12:47:39 -0400

    Fix the SHA

--- Formula/kontroll.rb
+++ Formula/kontroll.rb
@@ -2,7 +2,7 @@ class Kontroll < Formula
   desc "Keyboard layout switcher for ZSA keyboards"
   homepage "https://github.com/zsa/kontroll"
   url "https://github.com/zsa/kontroll/releases/download/1.0.4/kontroll-1.0.4-macos-universal.zip"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "fad2dde5d53114a8ff30ed75df414f169c13e00f04536e700c70785563811c5d"
   license "MIT"
 
   livecheck do
