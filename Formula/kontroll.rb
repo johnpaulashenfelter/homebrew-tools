5564b495cf47f6158c16dee8cd33e3e7fcd9bd30
John Paul Ashenfelter <john@ashenfelter.com>
2025-07-19 12:43:46 -0400

    Revise again based on strict check

--- Formula/kontroll.rb
+++ Formula/kontroll.rb
@@ -1,9 +1,14 @@
 class Kontroll < Formula
   desc "Keyboard layout switcher for ZSA keyboards"
   homepage "https://github.com/zsa/kontroll"
-  url "https://github.com/zsa/kontroll/releases/download/1.0.4/kontroll-1.0.4-macos-universal"
+  url "https://github.com/zsa/kontroll/releases/download/1.0.4/kontroll-1.0.4-macos-universal.zip"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
+
+  livecheck do
+    url "https://github.com/zsa/kontroll/releases/latest"
+    regex(%r{href=.*?/tag/(\d+(?:\.\d+)+)}i)
+  end
 
   def install
     bin.install "kontroll-1.0.4-macos-universal" => "kontroll"
@@ -11,10 +16,5 @@ class Kontroll < Formula
 
   test do
     system bin/"kontroll", "--version"
-  end
-
-  livecheck do
-    url "https://github.com/zsa/kontroll/releases/latest"
-    regex(%r{href=.*?/tag/(\d+(?:\.\d+)+)}i)
   end
 end
